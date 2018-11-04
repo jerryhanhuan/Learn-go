0fe023e3b9cf2c001701ff119b84a4e0286ee250
jerryhanhuan <dengfeng2012@163.com>
2018-11-04 22:24:29 +0800

    add goroutine test1

--- src/goroutine/go_routine_test1.go
+++ src/goroutine/go_routine_test1.go
@@ -5,6 +5,13 @@ import (
 	"runtime"
 )
 
+/*
+
+runtime.Gosched函数的作用是让当前正在运行的Goroutine（这里是运行main函数的那个Goroutine）暂时“休息”一下，
+而让Go运行时系统转去运行其它的Goroutine（这里是与go fmt.Println("Go!")对应并会封装fmt.Println("Go!")的那个Goroutine）。
+如此一来，我们就更加精细地控制了对几个Goroutine的运行的调度。
+ */
+
 func main(){
 
 	go fmt.Printf("Hello World")
